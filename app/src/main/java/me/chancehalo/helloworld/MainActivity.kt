4d31b838234acb99f9424e00718eb9132ece2ff0
halo <cembreyfarquhar@gmail.com>
2019-09-16 18:35:58 -0500

    Counting works!!!!!!

--- app/src/main/java/me/chancehalo/helloworld/MainActivity.kt
+++ app/src/main/java/me/chancehalo/helloworld/MainActivity.kt
@@ -2,13 +2,32 @@ package me.chancehalo.helloworld
 
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
+    private var mCount: Int = 0
+    private lateinit var mShowCount: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         Log.d("MainActivity", "Hello")
+        setContentView(R.layout.activity_main)
+        mShowCount = findViewById(R.id.show_count)
+    }
+
+    fun showToast(view: View) {
+        Toast.makeText(this, R.string.toast_message, Toast.LENGTH_SHORT).show()
+    }
+
+    fun countUp(view: View) {
+        mCount++
+        if (::mShowCount.isInitialized) {
+            mShowCount.setText(Integer.toString(mCount))
+        }
     }
 }
